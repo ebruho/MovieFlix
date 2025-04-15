81c1f869ea38ca583c4f061aace775237099f72e
Neli1612 <161248521+Neli1612@users.noreply.github.com>
2025-04-15 22:51:21 +0300

    add to tables

--- create.php
+++ create.php
@@ -1,5 +1,58 @@
 <?php
 include "config.php";
+
+$user="CREATE TABLE IF NOT EXISTS User (
+            user_id INT PRIMARY KEY,
+            username VARCHAR(255),
+            email VARCHAR(255),
+            user_password VARCHAR(255),
+            user_img VARCHAR(255),
+            joined_date TIMESTAMP
+        )";
+        $pdo->exec($user);
+
+        $admin="CREATE TABLE IF NOT EXISTS Admin (
+            admin_id INT PRIMARY KEY,
+            admin_name VARCHAR(255),
+            admin_email VARCHAR(255),
+            admin_password VARCHAR(255)
+        )";
+        $pdo->exec($admin);
+
+        $rating="CREATE TABLE IF NOT EXISTS Rating (
+            rating_id INT PRIMARY KEY,
+            rating_num INT,
+            comments VARCHAR(255),
+            user_id INT,
+            movie_id INT,
+            FOREIGN KEY (user_id) REFERENCES User(user_id),
+            FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+        )";
+        $pdo->exec($rating);
+
+        $watchlist="CREATE TABLE IF NOT EXISTS Watchlist (
+            watchlist_id INT PRIMARY KEY,
+            movie_id INT,
+            user_id INT,
+            added_date TIMESTAMP,
+            is_watched CHAR(1),
+            FOREIGN KEY (user_id) REFERENCES User(user_id),
+            FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+        )";
+        $pdo->exec($watchlist);
+        
+        $actor="CREATE TABLE IF NOT EXISTS Actor (
+            actor_id INT PRIMARY KEY,
+            actor_name VARCHAR(255)
+        )";
+        $pdo->exec($actor);
+
+        $director="CREATE TABLE IF NOT EXISTS Director (
+            director_id INT PRIMARY KEY,
+            director_name VARCHAR(255)
+        )";
+        $pdo->exec($director);
+
 $language="CREATE TABLE IF NOT EXISTS MovieLanguage (
             language_id INT PRIMARY KEY,
             language_name VARCHAR(255)
